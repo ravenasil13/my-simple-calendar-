919909d8c7fd1eb46e07b7123e423596106f967e
Raven Guerrero <ravenasil13@gmail.com>
2020-11-14 00:01:31 -0600

    added rows and began building CSS

---
+++ script.js
@@ -0,0 +1,16 @@
+//need a display at top of pg that shows present day
+var presentDay = moment().format("MMM Do YY");
+    $("#presentDay").format(presentDay);
+
+
+var actualHour = 
+
+
+
+
+
+
+
+
+//button that allows user to save task
+//functiion that shows current hour 
