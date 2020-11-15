d8c988e331dcd41a201c47563a1d9f988ce224fb
Raven Guerrero <ravenasil13@gmail.com>
2020-11-14 23:25:35 -0600

    gave id name to container, updated javascript

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-//need a display at top of pg that shows present day
-var presentDay = moment().format("MMM Do YY");
-    $("#presentDay").format(presentDay);
+//display at top of pg that shows present day
+var currentDay = moment().format("dddd MMMM Do YY");
+    $("#currentDay").text(currentDay);
 
 
-var actualHour = 
+//loop to save on local storage
+
+for (var i = 8; i < 17; i++) {
+    var input = $('<input class="input" data-id="input" data-hour="' +i +'"></input>')
+
+    var local = localStorage.getItem(i);
+    input.val(local);
+
+    var btn = $("<button class='saveBtn'>Save</button>")
+
+    input.append(input);
+    $("#box").append(input);
+}; 
 
 
 
 
-
-
-
-
-//button that allows user to save task
-//functiion that shows current hour 
